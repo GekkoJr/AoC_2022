ebc1ead2c55550bf10c08cc947991a869408c566
GekkoJr <gekkojr@proton.me>
2023-11-08 22:02:51 +0100

    day 4 complete (p1 & p2)

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,9 +1,23 @@
 use std::fs;
 
 fn main() {
-    let file: Vec<str> = fs::read_to_string("input.txt").unwrap().lines().collect();
+    let mut total: i32 = 0;
 
-    for line in file {
-        println!("{}", line);
+    for line in fs::read_to_string("input.txt").unwrap().lines() {
+        let elves: Vec<&str> = line.split(',').collect();
+        let elf_a: Vec<&str> = elves[0].split("-").collect();
+        let elf_b: Vec<&str> = elves[1].split("-").collect();
+
+
+        let a: i32 = elf_a[0].parse().unwrap();
+        let b: i32 = elf_a[1].parse().unwrap();
+        let c: i32 = elf_b[0].parse().unwrap();
+        let d: i32 = elf_b[1].parse().unwrap();
+
+        if ((a <= c && c <= b) || (a <= d && d <= b))  || ((c <= a && a <= d) || (c <= b && b <= d)) {
+            total += 1;
+        }
     }
+
+    println!("{}", total)
 }
