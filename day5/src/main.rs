35b226abdf3f8f82c3833e6630f873af67e4ead7
GekkoJr <gekkojr@proton.me>
2023-11-09 15:58:27 +0100

    day 5 has begun

---
+++ day5/src/main.rs
@@ -0,0 +1,30 @@
+use std::fs;
+
+fn main() {
+    let mut creates: Vec<Vec<char>> = vec![];
+
+    for _ in 0..10 {
+        creates.push(vec![]);
+    }
+
+    'wrapper: for line in fs::read_to_string("input.txt").unwrap().lines() {
+        let char_list: Vec<char> = line.chars().collect();
+        let mut new_char_list: Vec<char> = vec![];
+
+        let mut current_char = 0;
+        let mut not_empty: bool = true;
+        for c in char_list {
+            if c.is_numeric() {
+                break 'wrapper;
+            }
+            if !(c == '[' || c == ']') && not_empty {
+                new_char_list.push(c);
+                not_empty = false;
+            } else {
+                not_empty = true;
+            }
+        }
+
+        dbg!(new_char_list);
+    }
+}
