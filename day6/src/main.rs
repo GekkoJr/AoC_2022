5d815fd67156a9e8d4fe97599972a280b89750f1
GekkoJr <gekkojr@proton.me>
2023-11-10 10:41:34 +0100

    Day 6 complete

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,28 +1,52 @@
 use std::fs;
 
 fn main() {
-    let line = fs::read_to_string("test.txt").unwrap();
+    let line = fs::read_to_string("input.txt").unwrap();
 
     let mut current_chars: Vec<char> = vec![];
     let mut num = 0;
+    let mut total = 0;
 
     for i in line.chars() {
         num += 1;
-        if num <= 4 {
+        total += 1;
+        if num < 14 {
+
+            if current_chars.contains(&i) {
+                for x in 0..current_chars.len() {
+                    if i == current_chars[x] {
+                        for _ in 0..x+1 {
+                            current_chars.remove(0);
+                        }
+                        num -= x+1;
+
+                        break
+                    }
+                }
+            }
             current_chars.push(i);
-            continue;
         } else {
-            current_chars.remove(0);
+
             let tmp = current_chars.to_vec();
             dbg!(tmp);
 
 
             if !current_chars.contains(&i)
             {
-                println!("{}", num);
+                println!("{}", total);
                 break;
-            }
+            } else  {
+                for x in 0..current_chars.len() {
+                    if i == current_chars[x] {
+                        for _ in 0..x+1 {
+                            current_chars.remove(0);
+                        }
+                        num -= x+1;
 
+                        break
+                    }
+                }
+            }
             current_chars.push(i);
         }
     }
