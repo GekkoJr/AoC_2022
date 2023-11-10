2c4956523a98b889da402236ef59c5412da8d1fc
GekkoJr <gekkojr@proton.me>
2023-11-10 22:43:42 +0100

    almost complete

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -1,89 +1,88 @@
+use std::collections::HashMap;
 use std::fs;
-use std::thread::sleep;
 
 fn main() {
-    let mut dir_and_file: Vec<Vec<&str>> = vec![];
-    let mut current_dir: Vec<&str> = vec![];
+    let mut dir_list: HashMap<String, Vec<&str>> = HashMap::new();
+    let mut current_dir = vec!["main/"];
+    let mut line_read = false;
+    let mut current_lines: Vec<&str> = vec![];
+    // yes i hate this
 
-    let binding = fs::read_to_string("input.txt").unwrap();
-    for line in binding.lines() {
-        if line.contains('$') {
-            let command: Vec<&str> = line.split(" ").collect();
+    let bind_line = fs::read_to_string("test.txt").unwrap();
+    for line in bind_line.lines() {
+        let word_list: Vec<&str> = line.split(" ").collect();
 
-            if command[1] == "cd" {
-                if command[2] == "/" {
-                    current_dir = vec!["/"];
-                } else if command[2] == ".." {
-                    // this might be wrong
-                    if current_dir.len() != 0 {
+        if line.contains('$') && line_read {
+            line_read = false;
+            let string = String::from_iter(current_dir.clone());
+            if !dir_list.contains_key(&*string) {
+                dir_list.insert(string, current_lines.to_vec());
+            }
+
+            current_lines.clear();
+        }
+        // check if it is a command
+        if line.contains('$') && (word_list[1] == "cd") {
+            // if it is a cd , we dont care about ls here
+            let current_dir_length = current_dir.len();
+            match word_list[2] {
+                "/" => {
+                    current_dir.drain(1..current_dir_length);
+                }
+                ".." => {
+                    if current_dir_length > 0 {
+                        current_dir.pop();
                         current_dir.pop();
                     }
-                } else {
-                    current_dir.push(command[2])
+                }
+                _ => {
+                    current_dir.push(word_list[2]);
+                    current_dir.push("/")
                 }
             }
-        } else {
-            let mut already_indexed = false;
-            let pos = current_dir.len();
-            let mut over_pos = 0;
-            for i in dir_and_file.to_vec() {
-                if i[0] == current_dir[pos - 1] {
-                    already_indexed = true;
-                    let mut added = false;
-                    for x in i {
-                        if x == line {
-                            added = true
-                        }
-                        if !added {
-                            dir_and_file[over_pos].push(line);
-                            break;
-                        }
-                    }
-                }
+        }
+        if line.contains('$') && (word_list[1] == "ls") {
+            line_read = true;
+        }
 
-                over_pos += 1;
-            }
-            if !already_indexed {
-                let mut to_push: Vec<&str> = vec![];
-                to_push.push(current_dir.last().unwrap());
-                to_push.push(&line);
-
-                dir_and_file.push(to_push);
-            }
+        if !line.contains('$') {
+            current_lines.push(line);
         }
     }
 
-    for i in dir_and_file.to_vec() {
-        let mut skip = false;
-        let mut dir_size = 0;
-
-        dir_size = find_size(dir_and_file.to_vec(), i[0])
+    let mut totalt = 0;
+    for i in dir_list.keys() {
+        println!("{}", i);
+        let size = size_finder(dir_list.clone(), i.to_string());
+        println!("{size}");
+        if size <= 100000 {
+            totalt += size;
+            println!("adding")
+        }
+        println!("------------------")
     }
+    println!("{}", totalt)
 }
 
-
-fn find_size(dir_files: Vec<Vec<&str>>, dir: &str) -> i32 {
+fn size_finder(dir_list: HashMap<String, Vec<&str>>, path: String) -> i32 {
     let mut dir_size = 0;
+    dbg!(dir_list.clone());
 
-    for i in dir_files {
-        let mut skip = false;
-
-        if i[0] == dir {
-            for x in i {
-                line: Vec<&str> = x.split(" ").collect();
-                if skip {
-                    if line[0] == "dir" {
-                        dir_size += find_size(dir_files.to_vec(), line[1])
-                    } else {
-                        dir_size += line[0].parse::<i32>().unwrap();
-                    }
-                } else {
-                    skip = true;
-                }
+    if dir_list.clone().get(&path) != None {
+        for i in dir_list.get(&*path).unwrap() {
+            let line: Vec<&str> = i.split(" ").collect();
+            if line[0] == "dir" {
+                let p2 = line[1].to_string();
+                let new_path = format!("{path}{p2}");
+                println!("new path: {new_path}");
+                let to_add = size_finder(dir_list.clone(), new_path);
+                println!("{to_add}");
+                dir_size += to_add
+            } else {
+                dir_size += line[0].parse::<i32>().unwrap();
             }
-
         }
     }
 
-    return total_size;
+    return dir_size;
 }
---
+++ day7/src/main1.rs
@@ -0,0 +1,91 @@
+use std::fs;
+
+fn main() {
+    let mut dir_and_file: Vec<Vec<&str>> = vec![];
+    let mut current_dir: Vec<&str> = vec![];
+
+    let binding = fs::read_to_string("input.txt").unwrap();
+    for line in binding.lines() {
+        if line.contains('$') {
+            let command: Vec<&str> = line.split(" ").collect();
+
+            if command[1] == "cd" {
+                if command[2] == "/" {
+                    current_dir = vec!["/"];
+                } else if command[2] == ".." {
+                    // this might be wrong
+                    if current_dir.len() != 0 {
+                        current_dir.pop();
+                    }
+                } else {
+                    current_dir.push(command[2])
+                }
+            }
+        } else {
+            let mut already_indexed = false;
+            let pos = current_dir.len();
+            let mut over_pos = 0;
+            for i in dir_and_file.to_vec() {
+                if i[0] == current_dir[pos - 1] {
+                    already_indexed = true;
+                    let mut added = false;
+                    for x in i {
+                        if x == line {
+                            added = true
+                        }
+                        if !added {
+                            dir_and_file[over_pos].push(line);
+                            break;
+                        }
+                    }
+                }
+
+                over_pos += 1;
+            }
+            if !already_indexed {
+                let mut to_push: Vec<&str> = vec![];
+                to_push.push(current_dir.last().unwrap());
+                to_push.push(&line);
+
+                dir_and_file.push(to_push);
+            }
+        }
+    }
+
+    for i in dir_and_file.to_vec() {
+        let mut dir_size = 0;
+        dbg!(i);
+        // dir_size = find_size(&dir_and_file.to_vec(), i[0]);
+
+        println!("{}", dir_size)
+    }
+}
+
+
+fn find_size(dir_files: &Vec<Vec<&str>>, dir: &str) -> i32 {
+    let mut dir_size = 0;
+    println!("GO");
+
+    for i in dir_files {
+
+
+        if i[0] == dir {
+            let mut skip = false;
+            for x in i {
+                let line: Vec<&str> = x.split(" ").collect();
+                if skip {
+                    if line[0] == "dir" {
+                        dir_size += find_size(&dir_files.to_vec(), line[1])
+                    } else {
+                        dir_size += line[0].parse::<i32>().unwrap();
+                    }
+                } else {
+                    skip = true;
+                }
+            }
+
+        }
+    }
+
+    return dir_size;
+}
