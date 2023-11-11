8631e30d617ed12ff03ddf955e5331180f7f00b6
GekkoJr <gekkojr@proton.me>
2023-11-11 17:23:19 +0100

    day 8 p2

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -4,7 +4,10 @@ fn main() {
     let file = "input.txt";
     let data = parse(file);
 
-    let result = find_visible(data);
+    let result = find_visible(data.clone());
+    let p2 = find_score(data.clone());
+    println!("-------------------");
+    println!("highest score {p2}");
 
     println!("total visible: {result}")
 }
@@ -61,15 +64,12 @@ fn find_visible(data: Vec<Vec<i32>>) -> i32 {
                 } else if cl == lc {
                     if compare(highest_check, current_num) {
                         visible = true;
-                        println!("left")
                     }
                     highest_check = 0
                 }
-                println!("current highest {highest_check}")
             }
             if compare(highest_check, current_num) {
                 visible = true;
-                println!("right")
             }
             highest_check = 0;
             // checking colum
@@ -85,14 +85,12 @@ fn find_visible(data: Vec<Vec<i32>>) -> i32 {
                 } else if cc == rc {
                     if compare(highest_check, data[cc][cl]) {
                         visible = true;
-                        println!("top")
                     }
                     highest_check = 0
                 }
             }
             if compare(highest_check, data[cc][cl]) {
                 visible = true;
-                println!("bottom")
             }
             if visible {
                 total += 1;
@@ -111,4 +109,62 @@ fn compare(high: i32, x: i32) -> bool {
         return true;
     }
 
+}
+
+fn find_score(data: Vec<Vec<i32>>) -> i32 {
+    let mut highest_score: i32 = 0;
+
+    let mut col_index = 0;
+    for colum in data.clone() {
+        let mut num_index =0;
+        for num in &colum {
+            let mut top = 0;
+            let mut bottom = 0;
+            let mut left = 0;
+            let mut right = 0;
+
+            // left
+            for i in (0..num_index).rev() {
+                left += 1;
+                if colum[i] >= *num {
+                    break;
+                }
+
+            }
+
+            for i in (num_index+1)..colum.len() {
+                right += 1;
+                if colum[i] >= *num {
+                    break;
+                }
+            }
+
+            for i in (0..col_index).rev() {
+                top += 1;
+                if data[i][num_index] >= *num {
+                    break;
+                }
+            }
+
+            for i in (col_index+1)..data.len() {
+                bottom += 1;
+                if data[i][num_index] >= *num {
+                    break;
+                }
+
+            }
+
+            let score: i32 = (top * bottom * left * right).to_string().parse::<i32>().unwrap();
+
+            if score.to_string().parse::<i32>().unwrap() > highest_score {
+                highest_score = score;
+                println!("left {left} right {right} top {top} bottom {bottom}")
+            }
+            num_index += 1;
+        }
+        col_index += 1;
+    }
+
+
+    return highest_score;
 }
